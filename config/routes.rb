a7968e2ba49b70cc15c6698f5685afbac95835fa
Ureshino Misaki <2111140040a@ed.fuk.kindai.ac.jp>
2023-10-24 05:20:55 +0000

    push to render.com

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'top/main'
+  post 'top/login'
+  root 'top#main'
+  get 'top/logout'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
